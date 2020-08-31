f560c97ab79b7d1df48934d984dee850aec1084f
Sanmonsua <sanmonsua@gmail.com>
2020-08-30 19:28:22 -0500

    Fix : rows color palette changes every time is re render

--- App.js
+++ App.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { StyleSheet, Text, View, FlatList, Dimensions } from 'react-native';
 import MovieCard from './MovieCard'
 
+const colorPalettes = [
+  ["#84c0f0", "#83a4f8"], 
+  ["#d18de7", "#c63f86"],
+  ["#f5b166", "#f19f5a"],
+  ["#c97ef2", "#9b80f6"],
+  ["#68e8cc", "#65e2c3"],
+  ["#f06591", "#e45e7a"],
+]
+
 export default class App extends React.Component {
   
   state = {
@@ -10,8 +19,13 @@ export default class App extends React.Component {
     page : 1,
   }
 
-  renderItem = ({item}) =>(
-    <MovieCard item={item}/>
+  renderItem = ({item, index}) =>(
+    <MovieCard 
+      item={item} 
+      colors={
+        colorPalettes[index % colorPalettes.length]
+      }
+    />
   )
 
   componentDidMount = () =>{
--- MovieCard.js
+++ MovieCard.js
@@ -2,26 +2,8 @@ import React from 'react'
 import { View, Text, Image, StyleSheet} from 'react-native'
 import { LinearGradient } from 'expo-linear-gradient';
 
-const colorPalettes = [
-    ["#84c0f0", "#83a4f8"], 
-    ["#d18de7", "#c63f86"],
-    ["#f5b166", "#f19f5a"],
-    ["#c97ef2", "#9b80f6"],
-    ["#68e8cc", "#65e2c3"],
-    ["#f06591", "#e45e7a"],
-]
-
-let color = -1
-
-function pickColorPalette(){
-    color ++
-    if (color >= colorPalettes.length) color = 0
-    return colorPalettes[color]
-}
-
-
 export default MovieCard = (props) =>(
-    <LinearGradient colors={pickColorPalette()} style={styles.container}>
+    <LinearGradient colors={props.colors} style={styles.container}>
         <View>
             <Image
                 style={styles.poster}
